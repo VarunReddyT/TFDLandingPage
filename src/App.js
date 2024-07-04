48db3cedfa61b94ff4a04dcf9970ada50ec34dc8
Varun-MSI <tvarun2014@gmail.com>
2024-07-04 21:30:30 +0530

    New

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename='/games'>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
